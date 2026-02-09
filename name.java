534c15c2f05a0a347042256990fa553575c5fa88
zoya73 <zoyarukshar73@gmail.com>
2026-02-09 12:38:29 +0530

    Add basic Java program to greet user by name

---
+++ name.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class name {
+public static void main(String [] args)
+{
+    Scanner any = new Scanner (System.in);
+
+
+    System.out.print("Enter the name.");
+
+    String name = any.nextLine();
+
+    System.out.println("hello"+name);
+
+}
+    
+}
