6a596e165f1cd2dac4d383aa110f5eb7e7538555
zoya73 <zoyarukshar73@gmail.com>
2026-02-13 16:58:28 +0000

    multi

---
+++ Sq.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Sq{
+    public static void main( String []args) {
+        Scanner any = new Scanner(System.in);
+        System.out.print(" Enter the number you want print the multiple :");
+        int a = any.nextInt();
+
+        System.out.println(" The multiple of " +a);
+    for (int i=1; i<=10; i++){
+        System.out.println(+a+ " * "+i+" = "+ a*i);
+
+    }
+
+
+    }
+    
+    
+    }
