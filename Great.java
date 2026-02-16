03934c13c7a8be6385cfcc818bf44e7bfbb815d5
zoya73 <zoyarukshar73@gmail.com>
2026-02-16 14:57:28 +0000

    great

---
+++ Great.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Great{
+    public static void main ( String []args){
+        int a = 4;
+        int b = 7;
+
+        if (a>b){
+            System.out.println("The a is greater "+ a);
+        }
+        else {
+            System.out.println("The b is greater " +b);
+        }
+
+}
+    }
