081771765c3fbbdeb7b01f25d6e31c787b99be11
zoya73 <zoyarukshar73@gmail.com>
2026-03-04 10:07:52 +0000

    fir se yrr

---
+++ Msg.java
@@ -0,0 +1,15 @@
+class F1{
+    void msg1(){
+        System.out.print("hey\nthis is the diff class which only having the mgs in it\n  ");
+    }
+}
+
+
+public class Msg{
+    public static void main(String []args){
+        F1 obj = new F1();//create a object
+        obj.msg1();
+
+
+    }
+}
